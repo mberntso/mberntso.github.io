2da72867259ab6724cd57e77ebd7c7584de61536
Matthew Berntson <mdberntson@gmail.com>
2016-07-22 17:35:43 -0500

    edit js 1

--- src/surveyEx.js
+++ src/surveyEx.js
@@ -1,23 +1,29 @@
-var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-    { name:"page1", questions: [ 
-      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visible: false }
-    ]},
-    { name: "page2", questions: [
-      { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-      { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visible: false } ] },
-    { name: "page3",questions: [
-      { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
-  ],
-  triggers: [
-    { type: "visible", operator: "equal", value: "Yes", name: "frameworkUsing", questions: ["framework"]},
-    { type: "visible", operator: "equal", value: "Yes", name: "mvvmUsing", questions: ["mvvm"]}
-  ]
-}
-
-ReactDOM.render(
-  <ReactSurvey json={surveyJSON} onComplete={sendDataToServer}/>,
-  document.getElementById("surveyContainer"));
+var survey = new ReactSurveyModel({
+        triggers: [{ type: "visible", name: "type", operator: "equal", value: "Hot hatch", questions: ["Hot hatch"] },
+                { type: "visible", name: "type", value: "sedan", questions: ["Sedan"] },
+                { type: "visible", name: "type", value: "sports", questions: ["Sports"] },
+                { type: "visible", name: "type", value: "Grand tourer", questions: ["Grand tourer"] },                
+                { type: "visible", name: "type", value: "SuperCar", questions: ["Supercar"] },                
+                { type: "visible", name: "type", value: "Muscle car", questions: ["Muscle car"] },                
+                { type: "visible", name: "type", value: "Pony car", questions: ["Pony car"] },                
+                { type: "visible", name: "type", value: "Convertible", questions: ["Convertible"] },                
+                { type: "visible", name: "type", value: "other", questions: ["otherType"] }
+            ],
+        questions: [
+                { type: "radiogroup",  name: "type", isRequired: true, colCount: 4, hasOther: true,
+                title: "Please select the sport cars type.",
+                choices: ["Hot hatch", "sedan|Sports saloon / sports sedan", "sports|Sports Car", "Grand tourer", "SuperCar", "Muscle car", "Pony car", "Convertible"]},
+                { type: "radiogroup",  name: "Hot hatch", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["Honda Civic Type R", "Renault Megane RS", "Fiat 500 Abarth"]},
+                { type: "radiogroup",  name: "Sedan", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["BMW M5", "Mazdaspeed6/Mazda 6 MPS", "Dodge Charger", "Dodge SRT-4", "Lotus Cortina", "Mitsubishi EVO"]},
+                { type: "radiogroup",  name: "Sports", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["Chevrolet Corvette", "Mazda MX-5", "Porsche 911"]},
+                { type: "radiogroup",  name: "Grand tourer", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["Aston Martin V8", "Lexus SC300/400", "Ferrari 612 Scaglietti"]},
+                { type: "radiogroup",  name: "Supercar", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["McLaren P1", "Lamborghini Miura", "Bugatti Veyron 16.4"]},
+                { type: "radiogroup",  name: "Muscle car", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["Ford Torino", "Plymouth Road Runner", "Pontiac GTO", "Ford Falcon", "Holden Monaro", "Valiant Charger"]},
+                { type: "radiogroup",  name: "Pony car", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["AMC Javelin", "Chevrolet Camaro", "Dodge Challenger"]},
+                { type: "radiogroup",  name: "Convertible", isRequired: true, colCount: 4, visible: false, title: "Please select the car", hasOther: true, choices: ["Honda S2000", "Volkswagen Eos", "Volvo C70"]},
+                { type:"comment", name: "otherType", title: "Please describe the car.", isRequired: true, visible: false}
+            ]});
+ReactDOM.render(<ReactSurvey model={survey} onComplete={sendDataToServer}/>, document.getElementById("surveyElement"));
 
 function sendDataToServer(survey) {
   var resultAsString = JSON.stringify(survey.data);
