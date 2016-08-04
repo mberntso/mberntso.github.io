38107542ce58769099b14e03c9a75eee92bb3fe3
Matthew Berntson <mdberntson@gmail.com>
2016-08-04 12:56:09 -0500

    decision()

--- src/surveyEx.js
+++ src/surveyEx.js
@@ -10,21 +10,24 @@ var vertex3 = ["Solid Type","Solid1","Solid2"];
 // vertices must be placed into survey tree in Depth-First order
 var survey = [vertex1, vertex2, vertex3];
 
+var status = 0; // correllated with the index to use for the back button
+
 
 function begin() {
     document.body.removeChild(document.getElementById("intro"));
     document.body.removeChild(document.getElementById("begin"));
-    makeVertex(0);
+    makeVertex(status);
     
 }
 
 function makeVertex(index) {
     makeQuestion(index);
     makeButtons(index);
-    
+    status++;
 }
 
 function makeQuestion(index) {
+    // make question heading
     var para = document.createElement("H3");
     var t = document.createTextNode(survey[index][0]);
     para.appendChild(t);
@@ -33,10 +36,57 @@ function makeQuestion(index) {
 }
 
 function makeButtons(index) {
-    var para = document.createElement("BUTTON");
-    var t = document.createTextNode(survey[index][1]);
+    // make buttons for every option in the vertex
+    for (i = 1; i < survey[index].length; i++) { 
+        var para = document.createElement("BUTTON");
+        var t = document.createTextNode(survey[index][i]);
+        para.appendChild(t);
+        para.setAttribute("id", survey[index][i]);
+        para.setAttribute("onClick", "decision(index, i)");
+        document.body.appendChild(para);
+    }
+}
+
+function result(index, i) {
+    // prints result message
+    var para = document.createElement("H3");
+    var t = document.createTextNode(survey[index][0]);
     para.appendChild(t);
-    para.setAttribute("onClick", "decision()")
+    para.setAttribute("id", survey[index][0]);
     document.body.appendChild(para);
 }
+
+
+function backButton() {
+    // check status
+    if (status == 1) {
+
+    }
+    // run through math to make parent vertex
+
+
+}
+
+function removeVertex(index) {
+    // called by the decision(re: index) function
+
+
+}
+
+function decision(index, i) {
+    // make question heading
+    var para = document.createElement("H3");
+    var t = document.createTextNode(index+": "+i);
+    para.appendChild(t);
+    para.setAttribute("id", survey[index][0]);
+    document.body.appendChild(para);
+
+}
+
+
+
+
+
+
+
 
