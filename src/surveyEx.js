bf21eda42ffacbd1ef0b2eef2aa7fb969c604928
Matthew Berntson <mdberntson@gmail.com>
2016-08-06 14:21:27 -0500

    test panel text added except for Lymphoma and Lymphoid Leukemia

--- src/surveyEx.js
+++ src/surveyEx.js
@@ -14,30 +14,30 @@ var v8  = ["Glioma","DONE"];
 var v9  = ["Solid Tumor, NOS Result","DONE"];
 var v10 = ["Leukemia Type","Acute","Chronic"];
 var v11 = ["Lymphoma Result","DONE"];
-var v12 = ["Colorectal text"];
-var v13 = ["Lung text"];
+var v12 = ["Colorectal: (Minumum 20% tumor) MMR (if not already done) KRAS, BRAF, HRAS, NRAS, PIK3CA.  Notes: If absent marking for MLH-1 send the FFPB to M Health for hypermethylation testing."];
+var v13 = ["Lung: (Minumum 20% tumor) BRAF, EGFR, ERBB2, HRAS, JAK2, KRAS, MET, NRAS, PIK3CA; FISH: ALK, ROS1. Initial diagnosis, stage IIIB and IV, non-squamous non-small cell carcinomas.  Send the block of core tissue, resection tumor or fluid cell block to M Health molecular lab.  RET fusion test by FISH is being validated by cytogenetics loab as of April 2016. (RET positive tumor is treated with cabozantinib)"];
 var v14 = ["Fairview Case Result","DONE"];
 var v15 = ["Dr. Schuster Case Result","DONE"];
-var v16 = ["GIST text"];
-var v17 = ["Melanoma text"];
-var v18 = ["Glioma text"];
-var v19 = ["Solid tumor, NOS text"];
+var v16 = ["GIST: (Minimum 20% tumor) BRAF, KIT, PDGFRA."];
+var v17 = ["Melanoma: (Minimum 20% tumor) BRAF, KIT, HRAS, NRAS, PDGFRA."];
+var v18 = ["Glioma: (Minimum 20% tumor) IDH1, IDH2, TP53. New panel as of April 2016.  A separate molecular test for MGMT is in the verification stage as of April 2016. A separate FISH marker for 1p19q from the cytogenetics department can be ordered after seeing the results of the NGS panel."];
+var v19 = ["Solid tumor, NOS: (Minimum 20% tumor) BRAF, EGFR, ERBB2, HRAS, IDH!, IDH2, JAK2, KIT, KRAS, MET, NRAS, PDGFRA, PIK3CA, TP53."];
 var v20 = ["Acute Type","Myeloid","Lymphoid"];
 var v21 = ["Chronic: Myeloproliferative Neoplasm Panel","Complete","Short"];
-var v22 = ["Lymphoma text"];
-var v23 = ["Fairview case result text"];
-var v24 = ["Dr. Schuster text"];
+var v22 = ["Lymphoma: ."];
+var v23 = ["Thyroid Fairview Case: BRAF, HRAS, KRAS, NRAS, PIK3CA.  FNA samples must be collected in RPMI or DNA/RNA stabilization media. Send separate aspirate in RPMI cell preservative to M Health."];
+var v24 = ["Thyroid Dr. Schuster: UPMC panel: Point mutations and indels in the hotspots of genes: AKT1, BRAF, CTNNB1, EIF1AX, GNAS, HRAS, KRAS, PIK3CA, PTEN, RET, TERT, TP53 and TSR and 42 gene fusions involving genes: RET, PPARG, NTRK1, NTRK3, ALK, BRAF, and IGF2BP3. Send the aspirate to UPMC Presbyterian, Dept of Pathology, 200 Lathrap St, Pittsburgh, PA 15213, which is arranged through the M Health central cytology department."];
 var v25 = ["Acute Myeloid Panel","Complete","Short"];
 var v26 = ["Lymphoid Result","DONE"];
 var v27 = ["Complete Myeloproliferative Neoplasm Panel Result","DONE"];
 var v28 = ["Short Myeloproliferative Neoplasm Panel Result","DONE"];
 var v29 = ["Complete Acute Myeloid Panel Result","DONE"];
 var v30 = ["Short Acute Myeloid Panel Result","DONE"];
-var v31 = ["Lymphoid Result text"];
-var v32 = ["Complete Myeloproliferative Neoplasm Panel Result text"];
-var v33 = ["Short Myeloproliferative Neoplasm Panel Result text"];
-var v34 = ["Complete Acute Myeloid Panel Result text"];
-var v35 = ["Short Acute Myeloid Panel Result text"];
+var v31 = ["Lymphoid: ."];
+var v32 = ["Complete Myeloproliferative Neoplasm Panel: (blood or bone marrow only) (includes Molecular NS and rt-PCR BCR-ABL1) JAK@, CALR, MPL. BCR-ABL1 major quantitative (p210). BCR-ABL1 minor qualitative (p190)."];
+var v33 = ["Short Myeloproliferative Neoplasm Panel: (blood or bone marrow only) (includes Molecular NGS only) JAK2, CALR, MPL."];
+var v34 = ["Complete Acute Myeloid Panel: (Minimum 20% blast count required) FLT3, IDH1, IDH2, KIT, KRAS, NRAS, NPM1, PDGFRA, TP53, WT1. CEBPA (Minimum 30% blasts required)."];
+var v35 = ["Short Acute Myeloid Panel: (Minimum 20% blast count required) FLT3, NPM1, CEBPA."];
 
 // survey question vertices
 // vertices must be placed into survey tree in Depth-First order
@@ -147,7 +147,7 @@ function decision(i) {
 function result() {
   // prints result message using updated status
   // status should be a vertex with only one entry: the message
-  var para = document.createElement("H1");
+  var para = document.createElement("H2");
   var t = document.createTextNode(survey[status][0]);
   para.appendChild(t);
   para.setAttribute("id", survey[status][0]);
